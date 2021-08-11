2a5b1ab6c7e969c886a2f45906ebb0536ad5d89f
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-08-11 12:35:56 -0700

    added paraphrasing

--- first_app.py
+++ first_app.py
@@ -1,12 +1,17 @@
 import streamlit as st
+
+st.set_page_config(layout="wide")
+
 from multiapp import MultiApp
 
 import OCR
 import text_sum
 
+
+
 app = MultiApp()
 
 app.add_app("Optical Character Recognition", OCR.app)
-app.add_app("Text Summarization", text_sum.app)
+app.add_app("Text Summarization and Paraphrasing", text_sum.app)
 
 app.run()
--- text_sum.py
+++ text_sum.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
-#from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import streamlit as st
 
 @st.cache
@@ -11,10 +11,21 @@ def get_model():
 def get_tokenizer():
     tokenizer = T5Tokenizer.from_pretrained('t5-small')
     return tokenizer
+@st.cache
+def get_p_model():
+    model = AutoModelForSeq2SeqLM.from_pretrained("Vamsi/T5_Paraphrase_Paws")
+    return model
+@st.cache
+def get_p_tokenizer():
+    tokenizer = AutoTokenizer.from_pretrained("Vamsi/T5_Paraphrase_Paws")
+    return tokenizer
 
 tokenizer = get_tokenizer()
 model = get_model()
+p_tokenizer = get_p_tokenizer()
+p_model = get_p_model()
 
+@st.cache
 def preprocess(texts):
     preprocess_texts = texts.strip().replace("\n", "")
     texts1 = [preprocess_texts[n*512: (n+1)*512] for n in range(len(preprocess_texts) // 512)]
@@ -26,41 +37,108 @@ def preprocess(texts):
         tokens.append(tokenized_text)
     return tokens
 
+@st.cache
+def paraphrase(texts):
+
+    texts =  "paraphrase: " + texts
+
+    encoding = tokenizer.encode_plus(texts,padding='max_length', return_tensors="pt")
+    input_ids, attention_masks = encoding["input_ids"], encoding["attention_mask"]
+
+    outputs = model.generate(
+    input_ids=input_ids, attention_mask=attention_masks,
+    max_length=10000,
+    do_sample=True,
+    top_k=120,
+    top_p=0.95,
+    early_stopping=True,
+    num_return_sequences=5
+    )
+    lines = ""
+    for output in outputs:
+        line = tokenizer.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        lines += line
+    return lines
+
+@st.cache
+def get_summarization(tokenized_texts, min_length, max_length):
+    outputs = ""
+    for text in tokenized_texts:
+        summary_ids = model.generate(text,
+                                    num_beams=4,
+                                    no_repeat_ngram_size=2,
+                                    min_length=min_length,
+                                    max_length=max_length,
+                                    early_stopping=True)
+        output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        outputs += output
+    return outputs
+
+
 def app():
-    st.title("Text Summarization using Machine Learning and BERT")
-    bert = "BERT is designed to pre-train deep bidirectional representations from unlabeled text. it can be fine-tuned with just one additional output layer to create state-of-the-art models for a wide range of tasks, such as question answering and language inference, without substantial task-specific architecture modifications."
-    st.write(bert)
+    
+    st.title("Text Summarization and Paraphrasing using Machine Learning and BERT")
+    bert = """
+    - [Summarization and Paraphrasing] When choosing both summarization and paraphrasing option, the text will be summarized then paraphrased.
+    - [Length Selection] only applies to Summarization.
+    """
+    st.markdown(bert)
 
     col1, col2 = st.columns(2)
 
     with col1:
         st.subheader("Original")
         with st.form("Paste Here then Click the submit button"):
-            text = st.text_area("Paste the text you want summarized here, then click the submit button", height=250)
-            
-            tokenized_texts = preprocess(text)
 
-            selection = st.selectbox("Select the Length of Output", ["Short", "Medium", "Long"])
+            text = st.text_area("Paste the text you want summarized here, then click the submit button", height=250)
+
+            # Choosing whether to paraphrase or summarize
+            sorp = st.selectbox("Multiselect Paraphrase or Summarize", options=["Summarization and Paraphrasing", "Summarization Only", "Paraphrasing Only"])
             
-            if selection == "Short":
-                min_length, max_length = 10, 20
-            elif selection == "Medium":
-                min_length, max_length = 20, 35
-            elif selection == "Long":
-                min_length, max_length = 35, 100
+
+            if sorp == "Paraphrasing Only":
+                output = paraphrase(text)
+                
+            elif sorp == "Summarization and Paraphrasing":
+                # Choosing the length of summarization
+                selection = st.selectbox("Select the Length of Output", ["Short", "Medium", "Long"])
+                if selection == "Short":
+                    min_length, max_length = 10, 20
+                elif selection == "Medium":
+                    min_length, max_length = 20, 35
+                elif selection == "Long":
+                    min_length, max_length = 35, 100
             
-            outputs = ""
-            for text in tokenized_texts:
-                summary_ids = model.generate(text,
-                                    num_beams=4,
-                                    no_repeat_ngram_size=2,
-                                    min_length=min_length,
-                                    max_length=max_length,
-                                    early_stopping=True)
-                output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-                outputs += output
+                # Get output of summarization
+                tokenized_texts = preprocess(text)
+                outputs = get_summarization(tokenized_texts, min_length, max_length)
+
+                p = paraphrase(outputs)
+                output = p
+                
+            elif sorp == "Summarization Only":
+                # Choosing the length of summarization
+                selection = st.selectbox("Select the Length of Output", ["Short", "Medium", "Long"])
+                if selection == "Short":
+                    min_length, max_length = 10, 20
+                elif selection == "Medium":
+                    min_length, max_length = 20, 35
+                elif selection == "Long":
+                    min_length, max_length = 35, 100
+            
+                # Get output of summarization
+                tokenized_texts = preprocess(text)
+                outputs = get_summarization(tokenized_texts, min_length, max_length)
+                output = outputs
+                
+            else:
+                output = ""
+            
             submitted = st.form_submit_button("Submit")
+            # submit button
+            
+    
     with col2:
         st.subheader('Summarized')
         if submitted:
-            st.write(outputs)
+            st.write(output)
